092c4069e4ac5a9b2d94578a4a798bac5cfbd3e0
Brendson Silva <brendson.net@gmail.com>
2023-09-12 17:14:00 -0300

    bugfix: corrigindo bug no railway

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,13 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ mode }) => {
-    if (mode == 'production') {
-        return {
-            plugins: [vue()],
-            resolve: {
-                alias: {
-                    '@': fileURLToPath(new URL('./src', import.meta.url))
-                }
-            },
-            preview: {
-                port: 3000
-                // host: ''
+export default defineConfig(() => {
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
             }
-        };
-    } else {
-        return {
-            plugins: [vue()],
-            resolve: {
-                alias: {
-                    '@': fileURLToPath(new URL('./src', import.meta.url))
-                }
-            }
-        };
-    }
+        }
+    };
 });
