cad7b9952639c524be0d19154b10d4e3cf326f05
Brendson Silva <brendson.net@gmail.com>
2023-09-12 16:43:08 -0300

    bugfix: corrigindo bug no railway

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
                 }
             },
             preview: {
-                port: 8080
+                port: 3000
                 // host: ''
             }
         };
