355626a3985c7b0df45b7ff0b4630c41685861ae
Rafael Tomas <rafael.tomas@mv.com.br>
2023-10-14 18:14:21 -0300

    feat mask telefone

--- src/utils/maskTelefone.js
+++ src/utils/maskTelefone.js
@@ -3,5 +3,5 @@ export function maskTelefone(telefone) {
         telefone = telefone.replace(/[^\d]/g, '');
         return telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
     }
-    return '';
+    return '-';
 }
